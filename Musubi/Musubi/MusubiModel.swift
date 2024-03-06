32914fb69ac5ec815b995fc5f1c35513a3d836fb
tedklin <tedklin@berkeley.edu>
2024-03-05 23:10:23 -0700

    start redesigning local persistence to match git better

--- Musubi/Musubi/MusubiModel.swift
+++ Musubi/Musubi/MusubiModel.swift
@@ -5,15 +5,6 @@ import Foundation
 extension Musubi.Model {
     typealias HashPointer = Musubi.HashPointer
     
-    struct Repository: Codable {
-        let playlistID: Spotify.Model.ID
-        
-        // commit pointers
-        let head: HashPointer  // only one local head since branches are merged upon discovery.
-        let latestHubSync: HashPointer
-        let latestSpotifySync: HashPointer
-    }
-    
     // Hashable conformance here is only for SwiftUI List materialization,
     // NOT FOR STABLE ID ACROSS APP RUNS - for that, use `cryptoHash`.
     struct Commit: Codable, Hashable {
@@ -24,17 +15,12 @@ extension Musubi.Model {
         
         let parentCommits: [HashPointer]
         
-        // content pointers
-        let playlistNameHash: HashPointer
-        let playlistDescriptionHash: HashPointer
-        let playlistItemsHash: HashPointer
+        let rootTree: HashPointer
     }
     
-    // To enable persistence of "staging area" across app runs.
-    struct Playlist: Codable {
-        let id: Spotify.Model.ID
-        let name: String
-        let description: String
-        let items: [Spotify.Model.ID]
+    struct Tree {
+        let audioTrackListBlob: HashPointer
     }
+    
+    typealias AudioTrackList = [Spotify.Model.ID]
 }
--- Musubi/Musubi/Util/Storage.swift
+++ Musubi/Musubi/Util/Storage.swift
@@ -2,27 +2,12 @@
 
 import Foundation
 
-// TODO: a lot of repetition of userID, maybe wrap these static functions as methods of the user
-// instance / view model?
-
-// namespaces
-extension Musubi {
-    struct Storage {
-        private init() {}
-        
-        struct Keychain {
-            private init() {}
-        }
-        
-        struct Local {
-            private init() {}
-        }
-        
-        struct Remote {
-            private init() {}
-        }
-    }
-}
+// Hierarchy:
+//      appDir/userID/repoPlaylistID/
+//          objects/
+//          refs/remotes/
+//          HEAD
+//          index
 
 extension Musubi.Storage.Keychain {
     struct KeyIdentifier {
@@ -32,7 +17,7 @@ extension Musubi.Storage.Keychain {
             self.string = "com.musubi-app.keys.\(keyName.rawValue)"
         }
         
-        // TODO: support multiple users?
+        // TODO: support multiple concurrent users?
         // complicated by fact that we don't get user id until after successful oauth login
 //        init(keyName: KeyName, userID: Spotify.ID) {
 //            self.string = "com.musubi-app.keys.\(userID).\(keyName.rawValue)"
@@ -119,72 +104,21 @@ extension Musubi.Storage.Keychain {
     }
 }
 
-extension Musubi.Storage.Local {
-    // Hierarchy: `baseDir/userID/{repos or commits or stage}/objectID`
-    private static let baseDirURL = URL.libraryDirectory.appending(
-        path: "MusubiLocalStorage",
-        directoryHint: .isDirectory
-    )
-    private static let reposDirName = "repos"
-    private static let commitsDirName = "commits"
-    private static let stageDirName = "stage"
-    
-    private static func objectURL<T>(
-        type: T.Type,
-        objectID: String,
-        userID: Spotify.Model.ID
-    ) throws -> URL {
-        let dirName = switch type {
-            case is Musubi.Model.Repository.Type: reposDirName
-            case is Musubi.Model.Commit.Type: commitsDirName
-            case is Musubi.Model.Playlist.Type: stageDirName
-            default:
-                throw Musubi.StorageError.local(detail: "objectURL - unrecognized object type")
+// namespaces
+extension Musubi {
+    struct Storage {
+        private init() {}
+        
+        struct Local {
+            private init() {}
         }
-        return baseDirURL
-            .appending(path: userID, directoryHint: .isDirectory)
-            .appending(path: dirName, directoryHint: .isDirectory)
-            .appending(path: objectID, directoryHint: .notDirectory)
+        
+        struct Remote {
+            private init() {}
+        }
+        
+        struct Keychain {
+            private init() {}
+        }
     }
-    
-    static func save<T: Codable>(object: T, objectID: String, userID: Spotify.Model.ID) throws {
-        let objectData = try JSONEncoder().encode(object)
-        try objectData.write(
-            to: objectURL(type: T.self, objectID: objectID, userID: userID),
-            options: .atomic
-        )
-    }
-    
-    static func retrieve<T: Codable>(objectID: String, userID: Spotify.Model.ID) throws -> T {
-        return try JSONDecoder().decode(
-            T.self,
-            from: Data(contentsOf: objectURL(type: T.self, objectID: objectID, userID: userID))
-        )
-    }
-    
-    static func createDirs(userID: Spotify.Model.ID) throws {
-        try FileManager.default.createDirectory(
-            at: baseDirURL,
-            withIntermediateDirectories: false
-        )
-        try FileManager.default.createDirectory(
-            at: baseDirURL.appending(path: userID),
-            withIntermediateDirectories: false
-        )
-        try FileManager.default.createDirectory(
-            at: baseDirURL.appending(path: userID).appending(path: reposDirName),
-            withIntermediateDirectories: false
-        )
-        try FileManager.default.createDirectory(
-            at: baseDirURL.appending(path: userID).appending(path: commitsDirName),
-            withIntermediateDirectories: false
-        )
-        try FileManager.default.createDirectory(
-            at: baseDirURL.appending(path: userID).appending(path: stageDirName),
-            withIntermediateDirectories: false
-        )
-    }
-}
-
-extension Musubi.Storage.Remote {
 }
